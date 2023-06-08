04a859336c9d155ec3685abdc1461b203dc27ae8
naimur2001 <naimurrahman18thapril@gmail.com>
2023-06-08 20:28:53 +0600

    jwt added

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 require('dotenv').config()
 const express=require('express');
 const cors=require('cors');
+const jwt=require('jsonwebtoken');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const port=process.env.PORT || 5000;
 const app=express()
 //middleware
 app.use(express.json());
 app.use(cors());
+// jwt
+const verifyJWT=(req,res,next)=>{
+  const authorization=req.headers.authorization;
+  if(!authorization){
+    return res.status(401).send({error:true,message:'unauthorized access'})
+  }
+  const token=authorization.split(' ')[1];
+  // verify a token symmetric
+jwt.verify(token,process.env.JWT_TOKEN,(err,decoded)=>{
+  if (err) {
+    return res.status(403).send({error:true,message:'expired access'})
+  }
+  req.decoded=decoded;
+  next()
+}) 
+ 
+}
 // basic get  method---start
 app.get('/',(req,res)=>{
   res.send('Server is OK Dude.....')
@@ -38,6 +56,14 @@ async function run() {
   const usersCollection=client.db('cs-db').collection('users')
   const classesCollection=client.db('cs-db').collection('classes')
   const instructorsCollection=client.db('cs-db').collection('instructors')
+// jwt post
+
+app.post('/jwt',(req,res)=>{
+  const user=req.body;
+  const token=jwt.sign(user,process.env.JWT_TOKEN,
+    {expiresIn: '3h'});
+    res.send({token})
+})
 
 // user post
 app.post('/users',async(req,res)=>{
