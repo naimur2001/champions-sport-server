de7a10830f6aae0a1f1e950942dbc4f0be1b9d9d
naimur2001 <naimurrahman18thapril@gmail.com>
2023-06-08 14:17:35 +0600

    instructor get done

--- index.js
+++ index.js
@@ -62,6 +62,12 @@ app.get('/classes',async (req,res)=>{
   const result =await classesCollection.find().toArray();
   res.send(result);
 })
+//instructors get
+app.get('/instructors',async (req,res)=>{
+  const result =await instructorsCollection.find().toArray();
+  res.send(result);
+})
+   
  await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
